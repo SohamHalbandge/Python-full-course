cf9649c7cf8aa9c15c740e52c5a176aca94a1b6c
SohamHalbandge <113910105+SohamHalbandge@users.noreply.github.com>
2022-10-12 03:40:32 +0530

    Add files via upload

---
+++ Codes/boolean and logical operators.py
@@ -0,0 +1,26 @@
+print(5 == 5)
+print(5 == 6)
+print(6 == 6)
+print(type(True))
+print(type(False))
+print(type('True'))
+print(type(False))
+print(True + True)
+print(True - False)
+print(False * False + True)
+
+"""
+Different Types of Comparison Operators
+"""
+print(5 != 6)
+print(5 > 6)
+print(5 < 6)
+print(5 >= 6) 
+print(5 <= 6)
+print(5 is 6)
+print(5 is not 6)
+
+print()
+x = 32
+print(x > 0 and x < 10)
+print(not False)
